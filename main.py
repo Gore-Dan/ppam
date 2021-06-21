7e28683633d1da519a466c9cf9669807ce4b8c21
Gore-Dan <57291094+Gore-Dan@users.noreply.github.com>
2021-06-21 15:56:12 +0300

    updated

--- main.py
+++ main.py
@@ -5,51 +5,49 @@ import pyopencl as cl
 import os
 import cv2
 from natsort import natsorted
-
+from matplotlib import patches
+import matplotlib.pyplot as plt
+import time
 
 def getMSE(m1, m2):
     return np.square(np.subtract(m1, m2)).mean()
 
+start_time = time.time()
+dataset_name = 'football'
 
-videoFolder = './video/football.avi'
+#Generate frames for gpu
+typeVideo = '.avi'
+videoFolder = 'video/' + dataset_name + typeVideo
 videocap = cv2.VideoCapture(videoFolder)
 success, image = videocap.read()
+
 count = 0
 while success:
-    cv2.imwrite("./frames/fotbal/frame%d.jpg" % count, image)  # save frame as JPEG file
+    folderName = './frames/' + dataset_name + '/frame%d.jpg' % count
+    cv2.imwrite(folderName, image)  # save frame as JPEG file
     success, image = videocap.read()
     count += 1
 
-image_folder = './frames/fotbal'
-video_name = 'video.avi'
+image_folder = 'frames/' + dataset_name
 
+video_name = dataset_name + typeVideo
 fpsVideo = 25
+
+# reading frames generated before and sorting them by name
 images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
 images = natsorted(images)
 
-roiMingeFolderLocation = 'roi/cana.jpg'
-roiCanaFolderLocation = 'roi/cana.jpg'
-
-roiCanaImageData = cv2.imread(roiCanaFolderLocation).astype(np.uint8)
-heightCana, widthCana, layersCana = roiCanaImageData.shape
-
-globalFrame = cv2.imread('frames/fotbal/frame0.jpg', cv2.IMREAD_GRAYSCALE).astype(np.uint8)
+globalFrame = cv2.imread('./frames/' + dataset_name + '/frame0.jpg', cv2.IMREAD_GRAYSCALE).astype(np.uint8)
 globalheightFrame, globalwidthFrame = globalFrame.shape
-print(globalheightFrame, globalwidthFrame)
-# video = cv2.VideoWriter(video_name, 0, fpsVideo, (widthFrame, heightFrame))
 
-# for image in images:
-#     video.write(cv2.imread(os.path.join(image_folder, image)))
-#
-# cv2.destroyAllWindows()
-# video.release()
-
+# Section of code where we build the kernel
 ctx = cl.create_some_context()
 queue = cl.CommandQueue(ctx)
 
 mf = cl.mem_flags
 localTest = [images[0]]
 
+# the main logic is, we split the image we send in macroblocks and we calculate their MAD/MSE to find the ROI
 prg = cl.Program(ctx, """
 __kernel void calculateMSE(
     __global const uchar *frameData, 
@@ -69,27 +67,57 @@ __kernel void calculateMSE(
     for (int i = gidI; i < gidI + roiHeight; i++) {
         for (int j = gidJ; j < gidJ + roiWidth; j++) {
             int dif = frameData[(i*m)+j] - roiData[(i-gidI)*roiHeight+j-gidJ];
-            sum += dif*dif;
+            if(dif < 0){
+                dif = -dif;
+            }
+            sum += dif;
         }
     }
-
     resData[gid] = (float)sum/(roiHeight*roiWidth);
 }
 """).build()
-#TODO made if to choose between cup and ball
+
+# for img in images:
+#     currentFrameFolderLocation = os.path.join(image_folder, img)
+#     currentFrame = cv2.imread(currentFrameFolderLocation).astype(np.uint8)
+#     currentFramex = currentFrame.copy()
+#     # set blue and green channels to 0
+#     currentFramex[:, :, 0] = 0
+#     currentFramex[:, :, 1] = 0
+#     cv2.imwrite('./removed_channels/' + dataset_name + '/%s' % img, currentFramex)
+
+typeImage = '.jpg'
+roiFolderLocation = 'roi/' + dataset_name + typeImage
+
+roiImageData = cv2.imread(roiFolderLocation, cv2.IMREAD_GRAYSCALE).astype(np.uint8)
+heightRoi, widthRoi = roiImageData.shape
+
 stackResult = []
+
+if any(os.scandir('./removed_channels/' + dataset_name)):
+    print('not empty')
+    image_folder = './removed_channels/' + dataset_name
+    roiImageData = cv2.imread(roiFolderLocation).astype(np.uint8)
+
+    roiImageData = roiImageData.copy()
+    # set blue and green channels to 0
+    roiImageData[:, :, 0] = 0
+    roiImageData[:, :, 1] = 0
+
+    cv2.imwrite("./roi/minge_red.jpg", roiImageData)
+
+
 for image in images:
-    roiMingeImageData = cv2.imread(roiMingeFolderLocation, cv2.IMREAD_GRAYSCALE).astype(np.uint8)
-    heightMinge, widthMinge = roiMingeImageData.shape
     currentFrameFolderLocation = os.path.join(image_folder, image)
     currentFrame = cv2.imread(currentFrameFolderLocation, cv2.IMREAD_GRAYSCALE).astype(np.uint8)
+
     heightFrame, widthFrame = currentFrame.shape
 
-    mx = heightFrame - heightMinge
-    nx = widthFrame - widthMinge
+    mx = heightFrame - heightRoi
+    nx = widthFrame - widthRoi
 
     frameData = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=currentFrame)
-    roiData = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=roiMingeImageData)
+    roiData = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=roiImageData)
 
     dim = mx * nx
     fakeVector = np.zeros(dim).astype(np.float32)
@@ -97,8 +125,8 @@ for image in images:
     knl = prg.calculateMSE  # Use this Kernel object for repeated calls
 
     #convert data
-    heightData32 = np.int32(heightMinge)
-    widthData32 = np.int32(widthMinge)
+    heightData32 = np.int32(heightRoi)
+    widthData32 = np.int32(widthRoi)
     mData32 = np.int32(heightFrame)
     nData32 = np.int32(widthFrame)
 
@@ -113,6 +141,7 @@ def getMSEMin(stackResult):
     array = []
     for arr in stackResult:
         min = arr[0]
+        i = 0
         for (index, mse) in enumerate(arr):
             local = mse
             if local < min:
@@ -122,33 +151,106 @@ def getMSEMin(stackResult):
     return array
 
 
+
 def getCoordinates(index, frameHeight, frameWidth):
     x = index / frameHeight
     y = index % frameWidth
     return (round(x), round(y))
 
-minArray = getMSEMin(stackResult)
-for tuple in minArray:
-    print(getCoordinates(tuple[1], globalheightFrame, globalwidthFrame))
+minArray = getMSEMin(stackResult)  # Array with image and MSE min
+
+roiImageData = cv2.imread(roiFolderLocation, cv2.IMREAD_GRAYSCALE).astype(np.uint8)
+heightRoi, widthRoi = roiImageData.shape
+found_element_coordinates = []
+for pair in minArray:
+    coord = getCoordinates(pair[1], globalheightFrame - heightRoi, globalwidthFrame - widthRoi)
+    found_element_coordinates.append(coord)
+
+finalFolderLocation = 'final/' + dataset_name
+image_folder = 'frames/' + dataset_name
+images_with_object_located = []
+for index, image in enumerate(images):
+    current_image_path = os.path.join(image_folder, image)
+    img = cv2.imread(current_image_path, cv2.COLOR_BGR2RGB)
+    x, y = found_element_coordinates[index]
+    img_with_square = cv2.rectangle(img, (y, x), (y + widthRoi, x - heightRoi), (0, 255, 0), 2)
+    images_with_object_located.append(img_with_square)
+    cv2.imwrite(finalFolderLocation + '/frame%d.jpg' % index, img_with_square)
+
+
+# img_with_square = patches.Rectangle((y, x), heightMinge, widthMinge, linewidth=1, edgecolor='r', facecolor='none')
+# ax.imshow(img)
+# ax.add_patch(img_with_square)
+# plt.savefig('./final/fotbal/'+image)
+
+video = cv2.VideoWriter(video_name, 0, fpsVideo, (globalwidthFrame, globalheightFrame))
+for image in images:
+    video.write(cv2.imread(os.path.join(finalFolderLocation, image)))
+
+cv2.destroyAllWindows()
+video.release()
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
+start_time = time.time()
+
 # for testing
 # Check on CPU with Numpy:
-# for image in localTest:
+# cpu_results = []
+# processed_images = 0
+# for image in images:
 #     cpuArray = []
-#     frame = cv2.imread('roi/cana_mica_rau.jpg', cv2.IMREAD_GRAYSCALE).astype(np.int16)
+#     currentFrameFolderLocation = os.path.join(image_folder, image)
+#     frame = cv2.imread(currentFrameFolderLocation, cv2.IMREAD_GRAYSCALE).astype(np.int16)
 #     heightFrame, widthFrame = frame.shape
-#     #print(frame)
 #     roiMingeImageData = cv2.imread(roiMingeFolderLocation, cv2.IMREAD_GRAYSCALE).astype(np.int16)
 #     heightMinge, widthMinge = roiMingeImageData.shape
 #     for i in range(0, mx+1):
 #         for j in range(0, nx+1):
-#             localM1 = frame[i:i+widthMinge, j:j+heightMinge]
+#             localM1 = frame[i:i+heightMinge, j:j+widthMinge]
+#             # print(localM1.shape)
+#             # print(i, i + heightMinge, j, j + widthMinge)
 #             diff = localM1 - roiMingeImageData
 #             # mse = getMSE(localM1, roiMingeImageData)
-#             localSum = np.sum(np.square(diff))
+#             localSum = np.sum(np.abs(diff))
 #             cpuArray.append(localSum/(heightMinge*widthMinge))
 #
-# print(res_np)
-# print(cpuArray)
+#
+#     processed_images += 1
+#     print("Processed images: ", processed_images)
+#     cpu_results.append(cpuArray)
+#     minArrayCPU = getMSEMin(cpu_results)
+#     print(minArrayCPU)
+#
+# # print(res_np)
+#
+# print(minArray)  # GPU results
+# minArrayCPU = getMSEMin(cpu_results)
+# print(minArrayCPU)  # CPU results
+#
+#
+#
+# images_with_object_located = []
+# for index, image in enumerate(images):
+#     current_image_path = os.path.join(image_folder, image)
+#     img = cv2.imread(current_image_path, cv2.COLOR_BGR2RGB)
+#     x, y = found_element_coordinates[index]
+#     img_with_square = cv2.rectangle(img, (y, x), (y + heightMinge, x + widthMinge), (0, 255, 0), 2)
+#     images_with_object_located.append(img_with_square)
+#
+#     cv2.imwrite("./final/dot_cpu/frame%d.jpg" % index, img_with_square)
+#
+# video = cv2.VideoWriter('dot_cpu.avi', 0, fpsVideo, (globalwidthFrame, globalheightFrame))
+#
+# for image in images:
+#     video.write(cv2.imread(os.path.join("./final/dot_cpu/", image)))
+#
+# cv2.destroyAllWindows()
+# video.release()
+
+
+# print(np.linalg.norm(minArray - minArrayCPU))
+print("--- %s seconds ---" % (time.time() - start_time))
 # print(res_np - cpuArray)
 # print(np.linalg.norm(res_np - cpuArray))
 # assert np.allclose(res_np, cpuArray)
